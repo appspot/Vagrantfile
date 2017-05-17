071b20460614fefab82b40af71bf4a8de9407aac
Heng <ourwaiting@gmail.com>
2017-05-17 20:35:39 +0800

    实际测试过 bootstrap.sh 的执行，修复了一些细节上的问题

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,25 +30,26 @@ curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-
 echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-source ~/.bash_profile
+source ~/.bash_profileifconfig
 
 # use China pip source
 mkdir ~/.pip
 bash -c "cat /vagrant/pip.conf > ~/.pip/pip.conf"
 
 # install python
-pyenv install 3.5.1
+pyenv install 3.5.0
 
 # make develop folder, use Python 2.7.10, named '~/www', and Actived
-pyenv virtualenv 2.7.10 ~/www
-pyenv actived www
+cd ~
+pyenv virtualenv 3.5.0 www
+pyenv activate www
 
 # install python requirements
-cat > ~/www/requirements << __EOF__
+cat > ~/requirements << __EOF__
+redis
 mysql-python
 tornado
 wtforms
 pynsq
 __EOF__
-pip install -r ~/www/requirements -i http://mirrors.aliyun.com/pypi/simple
-
+pip install -r ~/requirements
